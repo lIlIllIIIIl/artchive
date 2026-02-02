f0ecde107b7fa13967418c55ac79b557817dab2b
lIlIllIIIIl <alexis.kessab@orange.fr>
2026-02-02 01:23:43 +0100

    feat: resolve deploy problems

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,6 +6,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: fileURLToPath(new URL('.', import.meta.url)),
   server: {
     port: 5173,
   },
